51a5918241f5cd6643ac3d1f858bfe1cac335dab
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-01-19 21:53:39 +0100

    build a service

---
+++ 01_OSGi_Basics/OSGiBasics/modules/helloworld-service/src/main/java/com/liferay/university/hello/impl/HelloServiceImpl.java
@@ -0,0 +1,24 @@
+package com.liferay.university.hello.impl;
+
+import com.liferay.university.hello.api.HelloService;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author BrandConstantin
+ */
+@Component(
+	immediate = true,
+	property = {
+		// TODO enter required service properties
+	},
+	service = HelloService.class
+)
+public class HelloServiceImpl implements HelloService {
+
+	@Override
+	public String hello(String parameter) {
+		return parameter ;
+	}
+
+}
